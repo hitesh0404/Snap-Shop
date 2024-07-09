e2b891ef0fe970266db4ba540ddefd6708746675
hitesh <hiteshkukaramprajapati@gmail.com>
2024-07-09 11:00:20 +0530

    ready with cart

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('add_to_cart/<int:id>',views.add_to_cart,name='add_to_cart'),
-    path('cart/',views.cart,name='cart')
+    path('increase_quantity/<int:id>/',views.increase_quantity,name='increase-quantity'),
+    path('decrease_quantity/<int:id>/',views.decrease_quantity,name='decrease-quantity'),
+    path('remove_product_from_cart/<int:id>/',views.remove_product_from_cart,name='remove-product-from-cart'),
+    path('clear_cart/',views.clear_cart,name='clear-cart'),
+    path('cart/',views.cart,name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,22 +1,35 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Cart,Customer,Product
 from django.contrib.auth.models import User
 
 
 
+def get_objects(request,id):
+    user_obj = User.objects.get(username=request.user)
+    cust_obj = Customer.objects.get(user=user_obj.id)
+    prod_obj = Product.objects.get(id=id)
+    return cust_obj,prod_obj
+
+
 
 # Create your views here.
 def add_to_cart(request,id):
-    user_id = User.objects.get(username=request.user)
-    customer_object = Customer.objects.get(user_id=user_id.id)
-    product_object = Product.objects.get(id=id)
-    print(customer_object)
-    item , create =Cart.objects.get_or_create(user=customer_object,product=product_object)
+    cust_obj,prod_obj = get_objects(request,id)
+
+    item , create =Cart.objects.get_or_create(user=cust_obj,product=prod_obj)
     if create:
         print('item created')
     else:
         item.quantity+=1
         item.save()
+    return cart(request,cust_obj)
+    
+
+
+def cart(request,customer_object='default'):
+    
+    if customer_object =='default':
+        customer_object= Customer.objects.get(user_id=request.user.id)
     cart = Cart.objects.filter(user=customer_object)
     context={
         'cart':cart
@@ -24,11 +37,35 @@ def add_to_cart(request,id):
     return render(request,'cart/cart.html',context)
 
 
-def cart(request):
-    customer_object = Customer.objects.get(user_id=request.user.id)
-    cart = Cart.objects.filter(user=customer_object)
-    context={
-        'cart':cart
-        }
-    return render(request,'cart/cart.html',context)
+def increase_quantity(request,id):
+    cust_obj,prod_obj = get_objects(request,id)
+    item = Cart.objects.get(user=cust_obj,product=prod_obj)
+    item.quantity+=1
+    item.save()
+    return cart(request,cust_obj)
+    
 
+
+def remove_product_from_cart(request,id):
+    cust_obj,prod_obj = get_objects(request,id)
+    item = Cart.objects.get(user=cust_obj,product=prod_obj)
+    item.delete()
+    return cart(request,cust_obj)
+    
+
+
+def decrease_quantity(request,id):
+    cust_obj,prod_obj = get_objects(request,id)
+    item = Cart.objects.get(user=cust_obj,product=prod_obj)
+    if item.quantity>1:
+        item.quantity-=1
+        item.save()
+    else:
+        item.delete()
+    return cart(request,cust_obj)
+
+
+def clear_cart(request):
+    cust_obj = Customer.objects.select_related('user').get(user=request.user.id)
+    Cart.objects.filter(user=cust_obj).delete()
+    return render(request,'cart/cart.html')
