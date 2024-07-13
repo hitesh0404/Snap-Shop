556d62279b62fb7c5dfd56bb85be77d94ac0272b
hitesh <hiteshkukaramprajapati@gmail.com>
2024-07-13 08:28:50 +0530

    supplier model

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer
+from .models import Customer,Supplier
 
 admin.site.register(Customer)
+admin.site.register(Supplier)
--- account/models.py
+++ account/models.py
@@ -12,3 +12,14 @@ class Customer(models.Model):
     user_create_date = models.DateTimeField(auto_now_add=True)
     profile_update_date= models.DateTimeField(auto_now=True,)
     gender = models.CharField(choices=choice,max_length=1)
+
+
+class Supplier(models.Model):
+    user  = models.OneToOneField(to=User,on_delete=models.CASCADE)
+    phone_number = models.BigIntegerField()
+    supplier_logo = models.ImageField(upload_to='supplier/logo/',default=r'C:\Users\admin\Django Project\Main Project Folder\media\images\product_image\random_cat.jpg')
+    user_create_date = models.DateTimeField(auto_now_add=True)
+    profile_update_date= models.DateTimeField(auto_now=True,)
+    document_photo =models.ImageField(upload_to='supplier/document/',default=r'C:\Users\admin\Django Project\Main Project Folder\media\images\product_image\random_cat.jpg')
+    company_name = models.CharField(max_length=30)
+
